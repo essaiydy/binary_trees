06dbe3d42211a3dbd7bbd8cfa16658559097a039
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-11-30 12:19:18 -0800

    task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -16,6 +16,6 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	return (binary_tree_height(tree->left)
-			- binary_tree_height(tree->right));
+	return ((int)(binary_tree_height(tree->left)
+			- binary_tree_height(tree->right)));
 }
