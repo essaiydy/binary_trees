a277df99d79a16a54623b5ac89e944cc96713e24
ayoub mezhar <ayoub.mezhar.usmba.ac.ma>
2023-11-30 19:57:12 +0100

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,13 +21,13 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	node = malloc(sizeof(binary_tree_t));
 
 	if (node == NULL)
+	{
 		return (NULL);
-
+	}
 	node->parent = parent;
 	node->n = value;
 	node->left = NULL;
 	node->right = NULL;
-
 	if (parent->left)
 	{
 		node->left = parent->left;
