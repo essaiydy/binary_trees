01beef0a3c5e65ce9cded8b39dbe355821670153
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-11-30 11:51:28 -0800

    17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,19 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * inary_tree_sibling - A  function that finds the sibling of a node
+ * binary_tree_sibling - A  function that finds the sibling of a node
  * @node: A pointer to the root of the binary tree
  * Return: A pointer to the sibling node;
  */
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (!node || !node->parent)
-        return (NULL);
+	if (!node || !node->parent)
+		return (NULL);
 
-    if (node->parent->left == node)
-    {
-        return (node->parent->right);
-    }
-    return (node->parent->left);
+	if (node->parent->left == node)
+	{
+		return (node->parent->right);
+	}
+	return (node->parent->left);
 }
