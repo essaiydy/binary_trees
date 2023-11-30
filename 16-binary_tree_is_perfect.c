73d1008f65eddc7d47cccbe8efb2a01318ff5502
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-11-30 11:55:03 -0800

    16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -17,7 +17,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 
 	if (binary_tree_height(tree->left)
-			== binary_tree_height(tree->right))
+			== binary_tree_height(tree->right) && tree->left
+			&& tree->right)
 		return (1);
 	return (0);
 }
