a3e4a5a58419467c43b2007c5f628634eceaa425
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-11-30 12:16:20 -0800

    task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,10 +13,11 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *node;
 
 	if (!parent)
 		return (NULL);
+	node = malloc(sizeof(binary_tree_t));
 	if (node == NULL)
 	{
 		return (NULL);
@@ -30,7 +31,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent->right)
 	{
 		node->right = parent->right;
-		parent->right->parent = node;
+		node->right->parent = node;
 
 	}
 	parent->right = node;
